e55ae2bc8f3ca2e1824fc2ab7eec4ea6caa8440e
hugoschaaf98 <hugoschaaf98@gmail.com>
2020-05-03 18:27:48 +0200

    first version. Only asks for enabling bluetooth

--- app/src/main/java/fr/damansoviet/stayonthebeat/MainActivity.java
+++ app/src/main/java/fr/damansoviet/stayonthebeat/MainActivity.java
@@ -1,19 +1,34 @@
 package fr.damansoviet.stayonthebeat;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.damansoviet.stayonthebeat.RoundKnobButton.RoundKnobButtonListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ConstraintLayout cl_main;
-    private LinearLayout ll_main;
+    private static final String TAG = "MainActivity";
+    // request code for enabling bluetooth
+    private static final int REQUEST_ENABLE_BT = 10;
+    // graphical components
+    private RelativeLayout rl_main;
+    private TextView tvBpm;
     private ImageButton ib_settings;
+    // peripherals
+    private BluetoothAdapter bluetoothAdapter;
+    // the metronome itself
+    private MetronomeSingleton metronome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +38,97 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void mInit() {
-        cl_main = (ConstraintLayout) findViewById(R.id.cl_main);
-        ll_main = (LinearLayout) findViewById(R.id.ll_main);
+        // bluetooth config
+        setupBluetooth();
+        // initialize and configure the metronome
+        metronome = MetronomeSingleton.getInstance();
+        metronome.configure(bluetoothAdapter);
+        // retrieve static graphical components
+        rl_main = (RelativeLayout) findViewById(R.id.rl_main);
         ib_settings = (ImageButton) findViewById(R.id.ib_settings);
-
-        //setContentView(ll_main);
+        // draw graphical components
+        drawBpmControl();
     }
 
-    private void drawRotaryKnob() {
+    private void setupBluetooth() {
 
+//        Intent iOpenBluetoothSettings = new Intent();
+//        iOpenBluetoothSettings.setAction(Settings.ACTION_BLUETOOTH_SETTINGS);
+//        startActivity(iOpenBluetoothSettings);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null) {
+            Toast.makeText(this,  "Your phone doesn't support bluetooth.",  Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            if(!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void drawBpmControl() {
+        // instantiate it
+        RoundKnobButton rv = new RoundKnobButton(this, R.drawable.stator, R.drawable.rotoron, R.drawable.rotoroff, 600, 600);
+        rv.setRotorPercentage(50f);
+        rv.setId(Utils.generateViewId());
+
+        // and draw it on screen
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+        rl_main.addView(rv, lp);
+
+        // update the bpm value of the metronome to match the rotary knob position
+        metronome.setBpmPercent(50f);
+
+        // place the value monitor above
+        tvBpm = new TextView(this);
+        tvBpm.setTextColor(Color.BLACK);
+        tvBpm.setGravity(Gravity.CENTER_HORIZONTAL);
+        tvBpm.setTextSize(18f);
+        tvBpm.setText(String.format("%d BPM", metronome.getBpm()));
+        lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.addRule(RelativeLayout.ABOVE,rv.getId());
+        lp.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        rl_main.addView(tvBpm, lp);
+
+        // set initial position and register listener to the rotary knob
+        rv.setListener(new RoundKnobButtonListener() {
+            public void onStateChange(boolean newState) {
+                Toast.makeText(MainActivity.this,  "Metronome "+(newState?"started":"paused"),  Toast.LENGTH_SHORT).show();
+                metronome.setState(newState);
+            }
+            public void onRotate(float percentage) {
+                metronome.setBpmPercent(percentage);
+                tvBpm.setText(String.format("%d BPM", metronome.getBpm()));
+            }
+        });
     }
 
     //*** Slots ***//
     public void openSettingsActivity(View v) {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
+        Intent iOpenSettings = new Intent(this, SettingsActivity.class);
+        startActivity(iOpenSettings);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case REQUEST_ENABLE_BT:
+                if(resultCode == RESULT_OK) {
+                    Log.i(TAG, "Bluetooth successfully enabled");
+                }
+                else {
+                    Toast.makeText(this, "You must enable bluetooth to use this app !", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            default:
+                Log.v(TAG, "Unknown requestCode.");
+        }
+    }
 }
--- app/src/main/java/fr/damansoviet/stayonthebeat/RoundKnobButton.java
+++ app/src/main/java/fr/damansoviet/stayonthebeat/RoundKnobButton.java
@@ -1,25 +1,16 @@
-package com.example.misctest;
-
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.PointF;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
-import android.widget.RelativeLayout;
-
 /*
+This file has been adapted by HugoS for StayOnTheBeat project.
+
+Modification Date:  April, 2020
+
+****************************************************************************
+
 File:              RoundKnobButton
 Version:           1.0.0
 Release Date:      November, 2013
 License:           GPL v2
 Description:	   A round knob button to control volume and toggle between two states
+
 
 ****************************************************************************
 Copyright (C) 2013 Radu Motisan  <radu.motisan@gmail.com>
@@ -40,37 +31,38 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ****************************************************************************/
+package fr.damansoviet.stayonthebeat;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.RelativeLayout;
 
 public class RoundKnobButton extends RelativeLayout implements OnGestureListener {
 
+	//*** members ***//
+	private static final String TAG = "RoundKnobButton";
 	private GestureDetector 	gestureDetector;
-	private float 				mAngleDown , mAngleUp;
+	private float 				mAngleDown , mAngleUp, percentage;
 	private ImageView			ivRotor;
 	private Bitmap 				bmpRotorOn , bmpRotorOff;
 	private boolean 			mState = false;
-	private int					m_nWidth = 0, m_nHeight = 0;
 	
 	interface RoundKnobButtonListener {
-		public void onStateChange(boolean newstate) ;
-		public void onRotate(int percentage);
+		public void onStateChange(boolean newState) ;
+		public void onRotate(float percentage);
 	}
-	
 	private RoundKnobButtonListener m_listener;
-	
-	public void SetListener(RoundKnobButtonListener l) {
-		m_listener = l;
-	}
-	
-	public void SetState(boolean state) {
-		mState = state;
-		ivRotor.setImageBitmap(state?bmpRotorOn:bmpRotorOff);
-	}
-	
-	public RoundKnobButton(Context context, int back, int rotoron, int rotoroff, final int w, final int h) {
+
+	//*** Constructors ***//
+	public RoundKnobButton(Context context, int back, int rotorOn, int rotorOff, final int w, final int h) {
 		super(context);
-		// we won't wait for our size to be calculated, we'll just store out fixed size
-		m_nWidth = w; 
-		m_nHeight = h;
 		// create stator
 		ImageView ivBack = new ImageView(context);
 		ivBack.setImageResource(back);
@@ -79,19 +71,19 @@ public class RoundKnobButton extends RelativeLayout implements OnGestureListener
 		lp_ivBack.addRule(RelativeLayout.CENTER_IN_PARENT);
 		addView(ivBack, lp_ivBack);
 		// load rotor images
-		Bitmap srcon = BitmapFactory.decodeResource(context.getResources(), rotoron);
-		Bitmap srcoff = BitmapFactory.decodeResource(context.getResources(), rotoroff);
-	    float scaleWidth = ((float) w) / srcon.getWidth();
-	    float scaleHeight = ((float) h) / srcon.getHeight();
+		Bitmap srcOn = BitmapFactory.decodeResource(context.getResources(), rotorOn);
+		Bitmap srcOff = BitmapFactory.decodeResource(context.getResources(), rotorOff);
+	    float scaleWidth = ((float) w) / srcOn.getWidth();
+	    float scaleHeight = ((float) h) / srcOn.getHeight();
 	    Matrix matrix = new Matrix();
 	    matrix.postScale(scaleWidth, scaleHeight);
 		    
 		bmpRotorOn = Bitmap.createBitmap(
-				srcon, 0, 0, 
-				srcon.getWidth(),srcon.getHeight() , matrix , true);
+				srcOn, 0, 0,
+				srcOn.getWidth(),srcOn.getHeight() , matrix , true);
 		bmpRotorOff = Bitmap.createBitmap(
-				srcoff, 0, 0, 
-				srcoff.getWidth(),srcoff.getHeight() , matrix , true);
+				srcOff, 0, 0,
+				srcOff.getWidth(),srcOff.getHeight() , matrix , true);
 		// create rotor
 		ivRotor = new ImageView(context);
 		ivRotor.setImageBitmap(bmpRotorOn);
@@ -99,30 +91,65 @@ public class RoundKnobButton extends RelativeLayout implements OnGestureListener
 		lp_ivKnob.addRule(RelativeLayout.CENTER_IN_PARENT);
 		addView(ivRotor, lp_ivKnob);
 		// set initial state
-		SetState(mState);
+		setState(mState);
 		// enable gesture detector
 		gestureDetector = new GestureDetector(getContext(), this);
 	}
-	
-	/**
-	 * math..
-	 * @param x
-	 * @param y
-	 * @return
-	 */
+
+	//*** private methods ***//
 	private float cartesianToPolar(float x, float y) {
 		return (float) -Math.toDegrees(Math.atan2(x - 0.5f, y - 0.5f));
 	}
 
-	
+	//*** Getters and setters ***//
+
+	public void setListener(RoundKnobButtonListener l) {
+		m_listener = l;
+	}
+
+	public boolean getState() { return mState; }
+
+	public void setState(boolean state) {
+		mState = state;
+		ivRotor.setImageBitmap(state?bmpRotorOn:bmpRotorOff);
+	}
+
+	private void setRotorPosAngle(float deg) {
+
+		if (deg >= 210 || deg <= 150) {
+			if (deg > 180) deg = deg - 360;
+			Matrix matrix=new Matrix();
+			ivRotor.setScaleType(ScaleType.MATRIX);
+			matrix.postRotate(deg, (int)(getWidth()/2), (int)(getHeight()/2));//;
+			ivRotor.setImageMatrix(matrix);
+		}
+	}
+
+	public float getRotorPercentage() {
+		return percentage;
+	}
+
+	public void setRotorPercentage(float percentage) {
+		// check if in bounds
+		this.percentage = (percentage<0f?0f:(percentage>100?100:percentage));
+		// compute degrees
+		float posDegree = this.percentage * 3f - 150f;
+		if (posDegree < 0) posDegree = 360f + posDegree;
+		setRotorPosAngle(posDegree);
+	}
+
+	//*** RelativeLayout part ***//
+
 	@Override public boolean onTouchEvent(MotionEvent event) {
 		if (gestureDetector.onTouchEvent(event)) return true;
 		else return super.onTouchEvent(event);
 	}
-	
-	public boolean onDown(MotionEvent event) {
-		float x = event.getX() / ((float) getWidth());
-		float y = event.getY() / ((float) getHeight());
+
+	//*** OnGestureListener implementation ***//
+
+	public boolean onDown(MotionEvent e) {
+		float x = e.getX() / ((float) getWidth());
+		float y = e.getY() / ((float) getHeight());
 		mAngleDown = cartesianToPolar(1 - x, 1 - y);// 1- to correct our custom axis direction
 		return true;
 	}
@@ -134,29 +161,11 @@ public class RoundKnobButton extends RelativeLayout implements OnGestureListener
 		
 		// if we click up the same place where we clicked down, it's just a button press
 		if (! Float.isNaN(mAngleDown) && ! Float.isNaN(mAngleUp) && Math.abs(mAngleUp-mAngleDown) < 10) {
-			SetState(!mState);
+			setState(!mState);
 			if (m_listener != null) m_listener.onStateChange(mState);
 		}
 		return true;
 	}
-
-	public void setRotorPosAngle(float deg) {
-
-		if (deg >= 210 || deg <= 150) {
-			if (deg > 180) deg = deg - 360;
-			Matrix matrix=new Matrix();
-			ivRotor.setScaleType(ScaleType.MATRIX);   
-			matrix.postRotate((float) deg, 210/2,210/2);//getWidth()/2, getHeight()/2);
-			ivRotor.setImageMatrix(matrix);
-		}
-	}
-	
-	public void setRotorPercentage(int percentage) {
-		int posDegree = percentage * 3 - 150;
-		if (posDegree < 0) posDegree = 360 + posDegree;
-		setRotorPosAngle(posDegree);
-	}
-	
 	
 	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
 		float x = e2.getX() / ((float) getWidth());
@@ -170,13 +179,13 @@ public class RoundKnobButton extends RelativeLayout implements OnGestureListener
 			
 			// deny full rotation, start start and stop point, and get a linear scale
 			if (posDegrees > 210 || posDegrees < 150) {
-				// rotate our imageview
+				// rotate our imageView
 				setRotorPosAngle(posDegrees);
 				// get a linear scale
 				float scaleDegrees = rotDegrees + 150; // given the current parameters, we go from 0 to 300
 				// get position percent
-				int percent = (int) (scaleDegrees / 3);
-				if (m_listener != null) m_listener.onRotate(percent);
+				percentage = scaleDegrees / 3;
+				if (m_listener != null) m_listener.onRotate(percentage);
 				return true; //consumed
 			} else
 				return false;
@@ -184,16 +193,9 @@ public class RoundKnobButton extends RelativeLayout implements OnGestureListener
 			return false; // not consumed
 	}
 
-	public void onShowPress(MotionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onShowPress(MotionEvent e) { 	}
 	public boolean onFling(MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) { return false; }
-
 	public void onLongPress(MotionEvent e) {	}
 
-	
-
-
-
 }
+
